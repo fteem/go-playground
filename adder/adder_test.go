33b348b0eb38b012368d49598899cd4bf11ca417
Ilija Eftimov <ilija@scribd.com>
2019-11-26 23:31:51 +0100

    Add Adder package

---
+++ adder/adder_test.go
@@ -0,0 +1,21 @@
+package adder
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	got := Add(2, 2)
+	want := 4
+
+	if got != want {
+		t.Errorf("Want '%d', got '%d'", want, got)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
