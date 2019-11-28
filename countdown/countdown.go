3c7b2748ead9ca4a3d91d03bd9eae3388c3823e6
Ilija Eftimov <ilija@scribd.com>
2019-11-28 17:01:00 +0100

    Add countdown package

---
+++ countdown/countdown.go
@@ -0,0 +1,29 @@
+package countdown
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+const countdownStart = 3
+const finalWord = "Go!"
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprintln(out, finalWord)
+}
---
+++ countdown/countdown_test.go
@@ -0,0 +1,35 @@
+package countdown
+
+import (
+	"bytes"
+	"testing"
+)
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func TestCountdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
+
+	Countdown(buffer, spySleeper)
+
+	got := buffer.String()
+	want := `3
+2
+1
+Go!
+`
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
+
+	if spySleeper.Calls != 4 {
+		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
+	}
+}
