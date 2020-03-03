03ff41abdd3565c8d1630195d5ed8182612f3dfe
Ilija Eftimov <ilija@scribd.com>
2020-03-03 01:10:04 +0100

    Fix error messages

--- snippetbin/main.go
+++ snippetbin/main.go
@@ -16,14 +16,14 @@ func getSnippets(db *gorm.DB) func(w http.ResponseWriter, r *http.Request) { // 
 	return func(w http.ResponseWriter, r *http.Request) {
 		var snippets []Snippet
 		if err := db.Find(&snippets).Error; err != nil {
-			fmt.Errorf("error retrieving snippets", err)
+			fmt.Errorf("error retrieving snippets: %s", err)
 			http.Error(w, err.Error(), http.StatusNotFound)
 			return
 		}
 
 		response, err := json.Marshal(snippets)
 		if err != nil {
-			fmt.Errorf("marshal error", err)
+			fmt.Errorf("marshal error: %s", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -40,14 +40,14 @@ func getSnippet(db *gorm.DB) func(w http.ResponseWriter, r *http.Request) {
 
 		var snippet Snippet
 		if err := db.First(&snippet, snippetID).Error; err != nil {
-			fmt.Errorf("error retrieving snippet", err)
+			fmt.Errorf("error retrieving snippet: %s", err)
 			http.Error(w, fmt.Sprintf("snippet with ID: %s not found", snippetID), http.StatusNotFound)
 			return
 		}
 
 		response, err := json.Marshal(snippet)
 		if err != nil {
-			fmt.Errorf("marshal error", err)
+			fmt.Errorf("marshal error: %s", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -63,20 +63,20 @@ func createSnippet(db *gorm.DB) func(w http.ResponseWriter, r *http.Request) {
 
 		err := json.NewDecoder(r.Body).Decode(&snippet)
 		if err != nil {
-			fmt.Errorf("Decode error: ", err)
+			fmt.Errorf("Decode error: %s", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
 		if err = db.Create(&snippet).Error; err != nil {
-			fmt.Errorf("Error creating snippet: ", err)
+			fmt.Errorf("Error creating snippet: %s", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
 		response, err := json.Marshal(snippet)
 		if err != nil {
-			fmt.Errorf("marshal error", err)
+			fmt.Errorf("marshal error: %s", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -96,27 +96,27 @@ func updateSnippet(db *gorm.DB) func(w http.ResponseWriter, r *http.Request) {
 		snippetID := vars["id"]
 
 		if err := db.First(&snippet, snippetID).Error; err != nil {
-			fmt.Errorf("error retrieving snippet", err)
+			fmt.Errorf("error retrieving snippet: %s", err)
 			http.Error(w, fmt.Sprintf("Snippet with ID: %s not found", snippetID), http.StatusNotFound)
 			return
 		}
 
 		err := json.NewDecoder(r.Body).Decode(&updated)
 		if err != nil {
-			fmt.Errorf("decode error: ", err)
+			fmt.Errorf("decode error: %s", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
 		if err := db.Model(&snippet).Updates(updated).Error; err != nil {
-			fmt.Errorf("Error updating snippet: ", err)
+			fmt.Errorf("Error updating snippet: %s", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
 		response, err := json.Marshal(snippet)
 		if err != nil {
-			fmt.Errorf("marshal error", err)
+			fmt.Errorf("marshal error: %s", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -134,20 +134,20 @@ func deleteSnippet(db *gorm.DB) func(w http.ResponseWriter, r *http.Request) {
 
 		var snippet Snippet
 		if err := db.First(&snippet, snippetID).Error; err != nil {
-			fmt.Errorf("error retrieving snippet", err)
+			fmt.Errorf("error retrieving snippet: %s", err)
 			http.Error(w, fmt.Sprintf("Snippet with ID: %s not found", snippetID), http.StatusNotFound)
 			return
 		}
 
 		if err := db.Delete(&snippet).Error; err != nil {
-			fmt.Errorf("error deleting snippet: ", err)
+			fmt.Errorf("error deleting snippet: %s", err)
 			http.Error(w, fmt.Sprint("Error deleting snippet: ", err), http.StatusBadRequest)
 			return
 		}
 
 		response, err := json.Marshal(snippet)
 		if err != nil {
-			fmt.Errorf("marshal error", err)
+			fmt.Errorf("marshal error: %s", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
